6e11a2c81c6d015a673a71d0ab8c16867d22bfed
Gustavo Felix <gustavi.felixdn@gmail.com>
2024-11-23 14:08:53 -0300

    Fixing expose port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Sucesso");
