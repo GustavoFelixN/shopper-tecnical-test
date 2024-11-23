c5213d0e23e3968127b0f10d03e961f3dcab9aa6
Gustavo Felix <gustavi.felixdn@gmail.com>
2024-11-23 13:38:13 -0300

    Adding hot realoading suppport

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
-app.listen(8080, () => {
-  console.log("server running on 8080");
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Sucesso");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server runnig at port: ${PORT}`);
 });
