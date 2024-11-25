415d5ad79868be1d99ef2023e16eeef57b189ad0
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-25 15:02:10 -0300

    changing view style

--- client/src/components/ViewOverlay/ViewOverlay.tsx
+++ client/src/components/ViewOverlay/ViewOverlay.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
+import { StyledViewOverlay } from './styles';
 
 interface ViewProps {
 	children: React.ReactNode,
 }
 
 const ViewOverlay: React.FC<ViewProps> = ({ children }) => {
-	return (
-		<div style={{
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			width: '33%',
-			height: '90vh',
-			backgroundColor: 'rgb(255, 255, 255, 0.8)',
-			padding: '20px',
-			margin: '25px',
-			borderRadius: '50px',
-			zIndex: 10,
-		}}>
-			{children}
-		</div>
-	);
+	return <StyledViewOverlay>{children}</StyledViewOverlay>;
 };
 
 export default ViewOverlay;
