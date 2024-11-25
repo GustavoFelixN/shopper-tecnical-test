86d1384cc1994e0c00075f2142cc1e5ccbfb5b74
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-25 16:47:05 -0300

    implementing simple validation to estimation form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,52 @@ function App() {
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
 
+  const [customerIdError, setCustomerIdError] = useState('');
+  const [originError, setOriginError] = useState('');
+  const [destinationError, setDestinationError] = useState('');
+
+  const clearErrors = () => {
+    setCustomerIdError('');
+    setOriginError('');
+    setDestinationError('');
+  }
+
+  const checkEmptyFields = () => {
+    if (customerId === '') {
+      setCustomerIdError('O ID não pode ser vazio');
+    }
+    if (origin === '') {
+      setOriginError('A origem não pode ser vazia');
+    }
+    if (destination === '') {
+      setDestinationError('O destino não pode ser vazio');
+    }
+
+    return customerId === '' || origin === '' || destination === '';
+
+  }
+
+  const checkSameAddress = () => {
+    if (origin.trim() === destination.trim()) {
+      setOriginError('Origem e destino devem ser diferente');
+      setDestinationError('Origem e destino devem ser diferente');
+      return true;
+    }
+    return false;
+  }
+
+  const handleSubmit = () => {
+    clearErrors();
+    const hasEmptyFields = checkEmptyFields();
+    if (hasEmptyFields) {
+      return
+    }
+    const hasSameAddress = checkSameAddress();
+    if (hasSameAddress) {
+      return
+    }
+  };
+
   return (
     <>
       <Map>
@@ -14,12 +60,10 @@ function App() {
           <FormView>
             <Input placeholder='ID' value={customerId} onChange={(e) => {
               setCustomerId(e.target.value)
-              if (e.target.value === 'popo') {
-              }
-            }} />
-            <Input placeholder='Origem' value={origin} onChange={(e) => { setOrigin(e.target.value) }} />
-            <Input placeholder='Destino' value={destination} onChange={(e) => { setDestination(e.target.value) }} />
-            <Button label='Estimar' onClick={() => { }} />
+            }} error={customerIdError} />
+            <Input placeholder='Origem' value={origin} onChange={(e) => { setOrigin(e.target.value) }} error={originError} />
+            <Input placeholder='Destino' value={destination} onChange={(e) => { setDestination(e.target.value) }} error={destinationError} />
+            <Button label='Estimar' onClick={handleSubmit} />
           </FormView>
         </ViewOverlay >
       </Map>
