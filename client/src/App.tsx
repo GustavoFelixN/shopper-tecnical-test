98efa3378eb4c91fff933a69693632d87714213f
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-25 10:38:16 -0300

    Adding Map component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import {default as Map} from './components/Map.tsx';
 
 function App() {
 
   return (
     <>
-      dale
+      <Map />
     </>
   )
 }
---
+++ client/src/components/Map.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect}  from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+const API_KEY = import.meta.env.GOOGLE_API_KEY;
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh',
+}
+
+const center = {
+  lat: -23.53,
+  lng: -46.78,
+}
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY,
+  })
+
+  useEffect(() => {
+    console.log(API_KEY);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={11}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  )
+}
+
+export default React.memo(Map)
