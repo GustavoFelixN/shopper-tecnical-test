fc90f75e700c4c86c7d5b027fb00637af60606d6
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-25 16:25:33 -0300

    Creating view for forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
-import { Map, ViewOverlay, Button, Input } from './components';
-
+import { Map, ViewOverlay, Button, Input, FormView } from './components';
+import { useState } from 'react';
 
 function App() {
+
+  const [customerId, setCustomerId] = useState('');
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
 
   return (
     <>
       <Map>
         <ViewOverlay >
-          <Input placeholder='' value='' onChange={(_e) => { }} />
-          <Button label='dale' onClick={() => { }} />
+          <FormView>
+            <Input placeholder='ID' value={customerId} onChange={(e) => {
+              setCustomerId(e.target.value)
+              if (e.target.value === 'popo') {
+              }
+            }} />
+            <Input placeholder='Origem' value={origin} onChange={(e) => { setOrigin(e.target.value) }} />
+            <Input placeholder='Destino' value={destination} onChange={(e) => { setDestination(e.target.value) }} />
+            <Button label='Estimar' onClick={() => { }} />
+          </FormView>
         </ViewOverlay >
       </Map>
     </>
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Map } from './Map/Map.tsx';
 export { default as Button } from './Button/Button.tsx';
 export { default as ViewOverlay } from './ViewOverlay/ViewOverlay.tsx';
 export { default as Input } from './Input/Input.tsx';
+export { default as FormView } from './FormView/FormView.tsx';
