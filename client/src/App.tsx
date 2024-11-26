b5f058d74a3bf2576823520adce4c15cdaaff180
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-26 12:46:44 -0300

    testing component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
-import { Map, EstimationForm } from './components';
+import { Map, DriversList } from './components';
+import { DriverCardProps } from './components/DriverCard/DriverCard.tsx';
 
 function App() {
 
+  const homer: DriverCardProps = {
+    id: 1,
+    name: 'Homer Simpson',
+    description: 'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).',
+    vehicle: 'Plymouth Valiant 1973 rosa e enferrujado',
+    review: {
+      rating: 2,
+      comment: 'Motorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.',
+    },
+    value: 2.5
+  }
+
+  const drivers = [homer, homer, homer, homer];
+
   return (
     <Map>
-      <EstimationForm />
+      <DriversList drivers={drivers} />
     </Map>
   );
 }
