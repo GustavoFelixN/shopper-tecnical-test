fb5cc66739b0f71915ff8d82bcb2337acd914dcc
Gustavo Felix <gustavo.felixdn@gmail.com>
2024-11-26 09:45:06 -0300

    Encapsulating form in component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,94 +1,10 @@
-import { Map, ViewOverlay, Button, Input, FormView } from './components';
-import { useState } from 'react';
+import { Map, EstimationForm } from './components';
 
 function App() {
-  const [customerId, setCustomerId] = useState('');
-  const [origin, setOrigin] = useState('');
-  const [destination, setDestination] = useState('');
-
-  const [customerIdError, setCustomerIdError] = useState('');
-  const [originError, setOriginError] = useState('');
-  const [destinationError, setDestinationError] = useState('');
-
-  const clearErrors = () => {
-    setCustomerIdError('');
-    setOriginError('');
-    setDestinationError('');
-  };
-
-  const checkEmptyFields = () => {
-    if (customerId === '') {
-      setCustomerIdError('O ID não pode ser vazio');
-    }
-    if (origin === '') {
-      setOriginError('A origem não pode ser vazia');
-    }
-    if (destination === '') {
-      setDestinationError('O destino não pode ser vazio');
-    }
-
-    return customerId === '' || origin === '' || destination === '';
-  };
-
-  const checkSameAddress = () => {
-    if (origin.trim() === destination.trim()) {
-      setOriginError('Origem e destino devem ser diferentes');
-      setDestinationError('Origem e destino devem ser diferentes');
-      return true;
-    }
-    return false;
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    clearErrors();
-    const hasEmptyFields = checkEmptyFields();
-    if (hasEmptyFields) {
-      return;
-    }
-    const hasSameAddress = checkSameAddress();
-    if (hasSameAddress) {
-      return;
-    }
-    // Lógica adicional ao enviar os dados
-    console.log('Formulário enviado com sucesso!');
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault(); // Evita o comportamento padrão de recarregar a página
-      handleSubmit(e);
-    }
-  };
 
   return (
     <Map>
-      <ViewOverlay>
-        <FormView>
-          {/* Adicione o evento onKeyDown no formulário */}
-          <form onKeyDown={handleKeyDown} onSubmit={handleSubmit}>
-            <Input
-              placeholder="ID"
-              value={customerId}
-              onChange={(e) => setCustomerId(e.target.value)}
-              error={customerIdError}
-            />
-            <Input
-              placeholder="Origem"
-              value={origin}
-              onChange={(e) => setOrigin(e.target.value)}
-              error={originError}
-            />
-            <Input
-              placeholder="Destino"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-              error={destinationError}
-            />
-            <Button label="Estimar" />
-          </form>
-        </FormView>
-      </ViewOverlay>
+      <EstimationForm />
     </Map>
   );
 }
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from './Button/Button.tsx';
 export { default as ViewOverlay } from './ViewOverlay/ViewOverlay.tsx';
 export { default as Input } from './Input/Input.tsx';
 export { default as FormView } from './FormView/FormView.tsx';
+export { default as EstimationForm } from './EstimationForm/EstimationForm.tsx';
