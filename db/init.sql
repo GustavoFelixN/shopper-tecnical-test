304196e94008e2920f740a7be15a30efd86dc197
Gustavo Felix <gustavi.felixdn@gmail.com>
2024-11-27 05:39:57 -0300

    adding drivers

--- db/init.sql
+++ db/init.sql
@@ -24,3 +24,7 @@ CREATE TABLE rides (
     FOREIGN KEY (driver_id) REFERENCES drivers(id)
 );
 
+INSERT INTO drivers (name, description vehicle, rating, review_comment, value, min_km) VALUES
+('Homer Simpson', 'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).', 'Plymouth Valiant 1973 rosa e enferrujado', 2, 'Motorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.', 2.5, 1),
+('Dominic Toretto', 'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada.', 'Dodge Charger R/T 1970 modificado', 4, 'Que viagem incrível! O carro é um show à parte e o motorista, apesar de ter uma cara de poucos amigos, foi super gente boa. Recomendo!', 5.0, 5),
+('James Bond', 'Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.', 'Aston Martin DB5 clássico', 5, 'Serviço impecável! O motorista é a própria definição de classe e o carro é simplesmente magnífico. Uma experiência digna de um agente secreto.', 10.0, 10);
